db7e1115e12b9fc669c4ac4d3957827ea280bce1
shule517 <shule517@gmail.com>
2016-07-12 00:06:45 +0900

    リファクタリング

--- draw-poker/draw-pokerTests/domain/StockTests.cs
+++ draw-poker/draw-pokerTests/domain/StockTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using draw_poker.domain;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace draw_poker.domain.Tests
 {
@@ -15,7 +10,7 @@ namespace draw_poker.domain.Tests
         public void カードは全部で52枚()
         {
             Stock stock = new Stock();
-            Assert.AreEqual(stock.Count, (13 * 4));
+            Assert.AreEqual((13 * 4), stock.Count);
         }
 
         [TestMethod()]
